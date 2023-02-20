68c98eff57c8c099fd6cd193ce2b009bdfdf850d
Bilous-Oleh <2005net2005@gmail.com>
2023-02-20 23:37:10 +0200

    Done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const siteNavEl = document.querySelector("#ingredients");
+console.log(siteNavEl);
+
+ingredients.forEach((element) => {
+  const navItemEl = document.createElement("li");
+  console.log(navItemEl);
+
+  navItemEl.classList.add("site-nav__item");
+  navItemEl.textContent = element;
+
+  siteNavEl.append(navItemEl);
+});
