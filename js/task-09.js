7f6cb2f23bb95e2efdc5c1f593fcc0e6d58b22a7
Bilous-Oleh <2005net2005@gmail.com>
2023-02-27 22:38:47 +0200

    Done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const colorEl = document.querySelector(".color");
+const btnChangeColorEl = document.querySelector(".change-color");
+
+btnChangeColorEl.addEventListener("click", (event) => {
+  document.body.style.background = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
