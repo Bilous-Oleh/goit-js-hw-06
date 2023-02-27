f4478667c6dead42d35fbbe368d9971e22c64a8e
Bilous-Oleh <2005net2005@gmail.com>
2023-02-27 23:35:56 +0200

    Improved task-03 and task-04

--- js/task-04.js
+++ js/task-04.js
@@ -3,7 +3,6 @@ let counterValue = 0;
 const btnDecrement = document.querySelector('[data-action="decrement"]');
 const btnIncrement = document.querySelector('[data-action="increment"]');
 const spanCounter = document.querySelector("#value");
-// console.log(btnIncrement.dataset);
 
 btnDecrement.addEventListener("click", onDecrementCounter);
 btnIncrement.addEventListener("click", onIncrementCounter);
